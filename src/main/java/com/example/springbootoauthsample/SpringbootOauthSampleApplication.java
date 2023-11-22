62899798a8860a793175fb364500f515cd44fe4a
Komichi Soudai <2101122@s.asojuku.ac.jp>
2023-11-22 11:36:09 +0900

    初期コミット

---
+++ src/main/java/com/example/springbootoauthsample/SpringbootOauthSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauthsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauthSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauthSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootoauthsample/SpringbootOauthSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauthsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauthSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
